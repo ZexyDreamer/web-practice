db75932deb097858a13ace676cf0e15f9c8de541
Valerii <36818754+ZexyDreamer@users.noreply.github.com>
2021-01-28 03:38:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = '';
+var mongoDB = 'Path to MongoDB';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
